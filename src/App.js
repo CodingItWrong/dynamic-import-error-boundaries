174974711c8ab2cc7afd2b7d1e93894997f3428e
Josh Justice <me@codingitwrong.com>
2024-01-08 12:25:59 -0500

    Add a successful dynamic load

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import {lazy} from 'react';
 import './style.css';
 
+const DynamicSuccess = lazy(() => import('./DynamicSuccess.js'));
+
 export default function App() {
-  return <h1>Hello, React</h1>;
+  return (
+    <div>
+      <h1>Hello, React</h1>
+      <p>This is the initial JS</p>
+      <DynamicSuccess />
+    </div>
+  );
 }
---
+++ src/DynamicSuccess.js
@@ -0,0 +1,3 @@
+export default function DynamicSuccess() {
+  return <p>This is the dynamically-loaded JS that should succeed.</p>;
+}
